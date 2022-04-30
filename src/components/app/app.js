0f4263abb06b3c710821b0c864fbce15d58fd065
Dhaval Chaudhary <dhavalchaudhary96@gmail.com>
2022-04-30 14:16:45 +0530

    added search box ui

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './app.css';
-import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
+import { InstantSearch, Hits } from 'react-instantsearch-dom';
 import { algoliaClient } from '../../config';
 import { ALGOLIA_INDEX_NAME } from '../../contants';
-
+import {Header} from'../header';
 
 export const App = () => {
   return (
     <InstantSearch searchClient={algoliaClient} indexName={ALGOLIA_INDEX_NAME}>
-      <SearchBox />
+      <Header />
       <Hits />
     </InstantSearch>
   );
---
+++ src/components/header/header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { SearchBox } from 'react-instantsearch-dom';
+import './header.css'
+
+export const Header = () => (
+    <header className="header">
+        <div className='content-width'>
+            <h1 className="header-title">
+                Restaurant Search Page
+            </h1>
+            <SearchBox
+                className="header-search-box"
+                translations={{
+                    placeholder: 'Search for a restaurant or cuisine',
+                }}
+            />
+        </div>
+    </header>
+)
