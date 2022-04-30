dcc62dca464cba5fec2f3c56b5d046107de7fafc
Dhaval Chaudhary <dhavalchaudhary96@gmail.com>
2022-04-30 14:11:40 +0530

    added algolia library

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './app.css';
+import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
+import { algoliaClient } from '../../config';
+import { ALGOLIA_INDEX_NAME } from '../../contants';
+
 
 export const App = () => {
   return (
-    <div>
-      Hello World
-    </div>
+    <InstantSearch searchClient={algoliaClient} indexName={ALGOLIA_INDEX_NAME}>
+      <SearchBox />
+      <Hits />
+    </InstantSearch>
   );
 }
